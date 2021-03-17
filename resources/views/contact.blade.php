c6011577c1388093d0da93e337206cda2581fca7
alvinaji503 <mca503@rajagiri.edu>
2021-03-17 12:47:22 +0530

    App

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,7 +16,7 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-      
+        <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="/">Student</a>
         </li>
         <li class="nav-item">
@@ -40,10 +41,8 @@
     </div>
   </div>
 </nav>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 <div class="container">
 <div class="row">
-<div class="col"></div>
 <div class="col">
 <table class="table">
     <tr>
@@ -51,15 +50,11 @@
         <td><input type="text" class="form-control"></td>
     </tr>
     <tr>
-        <td>Roll No</td>
+        <td>Message</td>
         <td><input type="text" class="form-control"></td>
     </tr>
     <tr>
-        <td>Adm no</td>
-        <td><input type="text" class="form-control"></td>
-    </tr>
-    <tr>
-        <td>college</td>
+        <td>Phone no</td>
         <td><input type="text" class="form-control"></td>
     </tr>
     <tr>
@@ -68,10 +63,9 @@
     </tr>
     </table>
 </div>
-<div class="col"></div>
 </div>
 </div>
-  
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    
 </body>
 </html>
--- resources/views/faculty.blade.php
+++ resources/views/faculty.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,7 +16,7 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-      
+        <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="/">Student</a>
         </li>
         <li class="nav-item">
@@ -40,26 +41,35 @@
     </div>
   </div>
 </nav>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 <div class="container">
 <div class="row">
-<div class="col"></div>
 <div class="col">
-<table class="table">
+</div>
+</div>
+</div>
+    <table class="table">
     <tr>
-        <td>Name</td>
+        <td>Faculty Name</td>
         <td><input type="text" class="form-control"></td>
     </tr>
     <tr>
-        <td>Roll No</td>
+        <td>Department</td>
         <td><input type="text" class="form-control"></td>
     </tr>
     <tr>
-        <td>Adm no</td>
+        <td>Address</td>
+        <td><textarea name="" id="" cols="20" rows="5" class="form-control"></textarea></td>
+    </tr>
+    <tr>
+        <td>Phone no</td>
         <td><input type="text" class="form-control"></td>
     </tr>
     <tr>
-        <td>college</td>
+        <td>Educational Qualification</td>
+        <td><textarea name="" id="" cols="20" rows="5" class="form-control"></textarea></td>
+    </tr>
+    <tr>
+        <td>College</td>
         <td><input type="text" class="form-control"></td>
     </tr>
     <tr>
@@ -67,11 +77,6 @@
         <td><button class="btn-btn-dark">Submit</button></td>
     </tr>
     </table>
-</div>
-<div class="col"></div>
-</div>
-</div>
-  
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
 </html>
